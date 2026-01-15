e5ad4097d968f8b9612809eb1fe5fb0c28959ffe
Ahmed Mohamed <162385026+AHMEDmoham3d@users.noreply.github.com>
2026-01-16 00:15:27 +0200

    first commit

--- src/components/Magazine.tsx
+++ src/components/Magazine.tsx
@@ -21,19 +21,19 @@ export default function Magazine() {
   const issues: MagazineIssue[] = [
     {
       id: 1,
-      title: 'أبناؤنا في الخارج ',
+      title: 'زيارة نادي سموحة',
       issue: 'العدد 15',
       date: 'يناير 2024',
       cover: '/تكريم-الرئيس.jpg',
-      description: 'في هذا العدد: مقابلة حصرية مع أبطالنا فى الخارج تغطية شاملة للإنجازات',
+      description: 'فيديو خاص بالزيارة لنادي سموحة',
     },
     {
       id: 2,
-      title: 'أبناء الصعيد ',
+      title: 'زيارة نادي سموحة',
       issue: 'العدد 14',
       date: 'ديسمبر 2023',
-      cover: 'ابناء الصعيد.jpg',
-      description: 'تغطية شاملة لبطولة الجمهورية، قصص النجاح، وملف خاص عن تطوير المواهب الشابة بالصعيد ',
+      cover: '/تكريم-الرئيس.jpg',
+      description: 'فيديو خاص بالزيارة لنادي سموحة https://www.facebook.com/share/v/1Dx7AAxAxz',
     },
     {
       id: 3,
